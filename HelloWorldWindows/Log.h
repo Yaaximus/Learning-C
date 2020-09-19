728cbc66f23f3eec849dee93e7bb74a64364c3ff
Yasiim Ahmad <yasim.ahmed63@yahoo.com>
2020-09-19 08:16:59 -0700

    Header files in C++

---
+++ HelloWorldWindows/Log.h
@@ -0,0 +1,28 @@
+// Header Guard
+#pragma once
+
+#include<iostream> // Angular brackets only for compiler include paths 
+//#include"../Log.h" //Quote for relative paths anywhere 
+
+// C standard libraries have .h extension whereas C++ donot
+
+// Header Guard(Legace)
+#ifndef _LOG_H
+#define _LOG_H
+
+void InitLog()
+{
+	std::cout << "Initializing Logging..." << std::endl;
+}
+
+#endif
+
+#ifndef _LOG_H
+#define _LOG_H
+
+void InitLog()
+{
+	std::cout << "Initializing Logging..." << std::endl;
+}
+
+#endif
