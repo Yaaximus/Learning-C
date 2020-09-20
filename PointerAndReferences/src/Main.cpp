5b7a95ea1af30a0bc999d4b87b03bb0d6e668c74
Yasim Ahmad <yasim.ahmed63@yahoo.com>
2020-09-20 03:35:11 -0700

    Pointers in C++

--- PointerAndReferences/src/Main.cpp
+++ PointerAndReferences/src/Main.cpp
@@ -3,6 +3,34 @@
 int main()
 {
 	std::cout << "Pointer and References" << std::endl;
+
+	// Null pointer
+	{
+		void* ptr = nullptr;
+		int temp_var = 8;
+		ptr = &temp_var;
+	}
+	
+	// Variable creation in stack
+	// Changing value of a address using pointer
+	{
+		int* ptr_1 = nullptr;
+		int temp_var_1 = 6;
+		ptr_1 = &temp_var_1;
+		*ptr_1 = 12;
+	}
+
+	// Variable creation at heap
+	{
+		char* buffer = new char[8];
+		memset(buffer, 0, 8);
+
+		char** ptr_2 = &buffer;
+
+		delete[] buffer;
+		delete ptr_2;
+	}
+
 	std::cout << "Press Enter to exit." << std::endl;
 	std::cin.get();
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Learning C++
 
-### Output Directory in Microsoft Visual Studion: $(SoluttionDir)bin\$(Platform)\$(Configuration)\
-### Intermediate Directory in Microsoft Visual Studion: $(SoluttionDir)bin\Intermediate\$(Platform)\$(Configuration)\
+#### Output Directory in Microsoft Visual Studion: $(SoluttionDir)bin\$(Platform)\$(Configuration)\
+#### Intermediate Directory in Microsoft Visual Studion: $(SoluttionDir)bin\Intermediate\$(Platform)\$(Configuration)\
