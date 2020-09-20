065e53390498439d97ac942f8cae18ec7263fa48
Yasim Ahmad <yasim.ahmed63@yahoo.com>
2020-09-20 04:17:41 -0700

    References in C++

--- PointerAndReferences/src/Main.cpp
+++ PointerAndReferences/src/Main.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 
+void IncrementUsingPointer(int* ptr)
+{
+	// Dereference the pointer first and then increment the value at the address
+	(*ptr)++;
+}
+
+void IncrementUsingReference(int& ref)
+{
+	ref++;
+}
+
 int main()
 {
 	std::cout << "Pointer and References" << std::endl;
@@ -28,8 +39,35 @@ int main()
 		char** ptr_2 = &buffer;
 
 		delete[] buffer;
-		delete ptr_2;
 	}
+
+	// Refrence(Its like an alias)
+	{
+		int temp_var = 2;
+		int& ref_temp_var = temp_var;
+
+		std::cout << temp_var << std::endl;
+		ref_temp_var = 4;
+		std::cout << temp_var << std::endl;
+	}
+
+	// Changing value in function using pointers
+	{
+		std::cout << "Changing value in function using pointers" << std::endl;
+		int temp_val = 3;
+		IncrementUsingPointer(&temp_val);
+		std::cout << temp_val << std::endl;
+	}
+	
+
+	// Changing value in function using References
+	{
+		std::cout << "Changing value in function using References" << std::endl;
+		int temp_val = 7;
+		IncrementUsingReference(temp_val);
+		std::cout << temp_val << std::endl;
+	}
+	
 
 	std::cout << "Press Enter to exit." << std::endl;
 	std::cin.get();
