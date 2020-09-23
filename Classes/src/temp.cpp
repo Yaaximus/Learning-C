5e581a659c3dcca31836477994ab26820f8328f4
Yaaximus <yasim.ahmed63@yahoo.com>
2020-09-23 20:14:15 +0500

    Enum implementation in Log class

--- Classes/src/temp.cpp
+++ Classes/src/temp.cpp
@@ -1,2 +1,17 @@
 static int s_Varialbe = 2;
 int s_Variable_externally_avaliable = 5;
+
+enum Example:char
+{
+	A, B, C
+};
+
+void temp_main()
+{
+	Example value = B;
+
+	if (value == 1)
+	{
+		// Do something here
+	}
+}
